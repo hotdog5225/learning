b7675065604b66c6ad184ffcf8b0ae6b715d8732
wuzewei.wzw <wuzewei.wzw@bytedance.com>
2021-11-12 14:06:38 +0800

    feat: python, spider, js reverse, encrypt code and phone_num

--- python/spider/bjguahao/crypt/decrypt.py
+++ python/spider/bjguahao/crypt/decrypt.py
@@ -1,6 +1,15 @@
-import base64
+import os
 
 if __name__ == '__main__':
-    code = '1318'
-    code_encrypt = ''
-    print(base64.b64decode(code.encode()))
+    # 组成调用js的命令
+    # node命令：node -e
+    cmd = 'node -e "require(\\"%s\\").init(%s,%s)"' % ('./crypt', 3, 5)
+    cmd2 = 'node -e "require(\\"%s\\").my_encrypt(%s)"' % ('./crypt', "123456")
+
+    pipeline = os.popen(cmd)
+    pipeline = os.popen(cmd2)
+
+    # 读取结果
+    result = pipeline.read()
+
+    print('结果是:', result)

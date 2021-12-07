7d3a505d837d7858d78803c846229d3ecf4f52a8
wuzewei.wzw <wuzewei.wzw@bytedance.com>
2021-12-07 08:01:50 +0800

    feat: c++, inheritance, access specifiers

--- cpp/learn_cpp/inheritance/access_specifier.cpp
+++ cpp/learn_cpp/inheritance/access_specifier.cpp
@@ -16,5 +16,22 @@ public:
     int m_public{};
 };
 
-class Derived: public Base {
+// public inheritance:
+// - public     -> public (can be accessed by anybody)
+// - protected  -> protected
+// - private    -> inaccessible
+class Pub: public Base {
 };
+
+// protected inheritance:
+// - public     -> protected (only can be accessed by Member, Friend and Derived)
+// - protected  -> protected
+// - private    -> inaccessible
+class Pro: protected Base {
+};
+
+// protected private:
+// - public     -> private (can only be accessed by Member, Friend)
+// - protected  -> private
+// - private    -> inaccessible
+class Pri: private Base{};
